deccd234833ee5541f9bccdc50969ec11a8a45f1
Grey Hardy <greyhardy7@gmail.com>
2021-10-01 15:34:12 -0400

    test

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiaXRzZ3JleWVkb3V0IiwiYSI6ImNrdTdteDl2bjBpeG0ycm1ubDYzb3RhZmwifQ.sO0NnCMXsXceYGW6EyqHSA";
---
+++ static/js/dist/config.dev.js
@@ -0,0 +1,4 @@
+"use strict";
+
+// API key
+var API_KEY = "pk.eyJ1IjoiaXRzZ3JleWVkb3V0IiwiYSI6ImNrdTdteDl2bjBpeG0ycm1ubDYzb3RhZmwifQ.sO0NnCMXsXceYGW6EyqHSA";
